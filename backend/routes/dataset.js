fe97c4dc62a56197c977af68a0106c9587bf62e7
wychauae <wychauae@connect.ust.hk>
2024-05-15 20:48:16 +0800

    fix: remove extra route

--- backend/routes/dataset.js
+++ backend/routes/dataset.js
@@ -4,7 +4,7 @@ const authentication = require('../middleware/authentication')
 const upload = require('../middleware/upload')
 router = express.Router()
 
-router.post('/uploadDataset', authentication.verifyToken, upload.single('dataset'), uploadDataset)
+router.post('/', authentication.verifyToken, upload.single('dataset'), uploadDataset)
 
 
 module.exports = router
