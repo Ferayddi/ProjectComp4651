1a35512d97b269fc8fb50cfcaf78c31ac9445e76
mattc <mattchoi722@gail.com>
2024-05-16 10:59:27 +0800

    handle all 3 anaylsis

--- backend/routes/analysis.js
+++ backend/routes/analysis.js
@@ -1,13 +1,9 @@
-const express = require('express')
-const {quickresult} = require('../controllers/neranalysisController')
-const {datasetAnalysis} = require('../controllers/datasetAnalysisController')
+const express = require("express");
+const { quickresult } = require("../controllers/neranalysisController");
+const { datasetAnalysis } = require("../controllers/datasetAnalysisController");
+const authentication = require("../middleware/authentication");
+router = express.Router();
 
-const authentication = require('../middleware/authentication')
-router = express.Router()
-
-
-router.post('/new', authentication.verifyToken, datasetAnalysis)
-
-
-module.exports = router
-module.exports.router = express()
+router.post("/new", authentication.verifyToken, datasetAnalysis);
+module.exports = router;
+module.exports.router = express();
